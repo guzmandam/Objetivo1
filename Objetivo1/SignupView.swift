4556619ec1ab02646908f20f2efeb563a66d4b73
Andres <320156564@pcpuma.acatlan.unam.mx>
2023-04-24 10:40:31 -0600

    NEW: Primer cambio

---
+++ Objetivo1/SignupView.swift
@@ -0,0 +1,21 @@
+//
+//  SignupView.swift
+//  Objetivo1
+//
+//  Created by CEDAM32 on 24/04/23.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    
+    var body: some View {
+        
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
