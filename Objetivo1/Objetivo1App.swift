764f0f707cee3ec9aa2ca242ef5ebd185de9a027
Andres <320156564@pcpuma.acatlan.unam.mx>
2023-04-24 10:36:58 -0600

    Initial Commit

---
+++ Objetivo1/Objetivo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Objetivo1App.swift
+//  Objetivo1
+//
+//  Created by CEDAM32 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Objetivo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
